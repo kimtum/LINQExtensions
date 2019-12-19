4fb4582931c6bc3ccdc3cee4ff43e340f3552a23
kim.tum <kim.99.tumanyan@gmail.com>
2019-12-20 00:49:29 +0400

    Add project files.

---
+++ LINQExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LINQExtentions
+{
+    public static class LINQExtensions
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>
+            (this IEnumerable<TSource> source, Func<TSource, TResult> selector) 
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (selector == null)
+                throw new ArgumentNullException("selector");
+            return (from item in source select selector(item));
+            
+        }
+
+
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            foreach (var item in source)
+            {
+                if(predicate(item))
+                    yield return item;
+            }
+        }
+
+
+        public static IEnumerable<IGrouping<TKey, TSource>> MyGroupBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) 
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (keySelector == null)
+                throw new ArgumentNullException("keySelector");
+            return (from item in source group item by keySelector(item));
+
+        }
+
+
+        public static List<TSource> MyToList<TSource>(this IEnumerable<TSource> source) 
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            List<TSource> mylist = new List<TSource>();
+            foreach (var item in source)
+                mylist.Add(item);
+            return mylist;
+
+        }
+
+
+        public static IOrderedEnumerable<TSource> MyOrderBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) 
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (keySelector == null)
+                throw new ArgumentNullException("keySelector");
+            return (from item in source orderby keySelector(item) select item);
+        }
+
+        public static IOrderedEnumerable<TSource> MyOrderByDescending<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            return source.OrderByDescending(y => keySelector(y));
+        }
+
+
+        public static Dictionary<TKey, TSource> MyToDictionary<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            return source.ToDictionary(item => keySelector(item), item => item);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExtentions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new[] { "London", "Paris" };
+            var customers = new[]
+            {
+                new { Name = "John", City = "London", Age = 24 },
+                new { Name = "Marinette", City = "Paris", Age = 31  },
+                new { Name = "Pier", City = "Paris", Age = 49  },
+                new { Name = "Diego", City = "Madrid", Age = 28  },
+                new { Name = "Ivan", City = "Moscow", Age = 35  },
+                new { Name = "Boris", City = "London", Age = 56  },
+            };
+
+            var x = customers.MyToList().MyOrderByDescending(y => y.Age).MyWhere(y => y.Age > 20).MySelect(y => y.Name);
+            //var query = customers.MyToDictionary(y => y.Name);
+            //foreach(var a in query)
+            //    Console.WriteLine($"{a.Key}: {a.Value}");
+            foreach(var a in x)
+                Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
